709c5ce0141a844d8d739bade59c415d8869c342
revanthkumaar <iamrevanthkumar@gmail.com>
2021-03-22 15:23:43 +0530

    lsdkflks
    df;aslkd

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+
+  test("the count must be 0 initially", () => {
+    const wrapper = shallow(<App />);
+    const text = wrapper.find("p").text();
+    expect(text).toEqual("Count: 0");
+  });
+
+  test("increment count by 1 when increment button is clicked", () => {
+    const wrapper = shallow(<App />);
+    const incrementButton = wrapper.find(button.increment);
+    incrementButton.simulate("click");
+    const text = wrapper.find("p").text();
+    expect(text).toEqual("Count: 1");
+  }
+
+
+
 });
